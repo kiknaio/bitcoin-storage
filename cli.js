1683f8f1d5f2b72c56edd648c1fd1dcccf4d580f
George Kiknadze <g.kiknadze@hotmail.com>
2018-07-08 14:06:50 +0200

    generate qr code for address

--- cli.js
+++ cli.js
@@ -1,10 +1,11 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --no-deprecation
 const program = require('commander');
 const bitcore = require('bitcore-lib');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const crypto = require('crypto');
 const path = require('path');
+const boxen = require('boxen');
 const wallets = require('./wallets');
 
 const generatePrivateKey = livenet => {
@@ -15,9 +16,9 @@ const generatePrivateKey = livenet => {
 
 program
   .version('0.1.0')
-  .option('-p, --privatekey', 'Generate new Private Key')
   .option('-l, --livenet', 'For Livenet. Default is Testnet')
   .option('-q, --qrcode', 'Generate address QR code')
+  .option('-a, --address', 'Display address')
 
 // Generate Private Key
 program
@@ -26,16 +27,25 @@ program
   .action(() => {
 
     // Generate private key
-    const key = generatePrivateKey(program.livenet).toObject();
+    const key = generatePrivateKey(program.livenet);
+
+    // Display (-a) Address
+    if (program.address) {
+      console.log(boxen(`Address: ${key.toAddress().toString()} - ${program.livenet ? 'Livenet' : 'Testnet'}`, {padding: 1}));
+    }
+    // Display (-q) QR code for address
+    if (program.qrcode) {
+      qrcode.generate(key.toAddress().toString());
+    }
 
     // Generate random hash for private key file
     const randomHash = crypto.createHash('sha256')
       .update((new Date()).valueOf().toString() + Math.random().toString).digest('hex');
 
     // Save private key as a file
-    fs.writeFile(path.resolve(__dirname, `./keychain/${randomHash}.json`), JSON.stringify(key), 'utf8', (err, done) => {
+    fs.writeFile(path.resolve(__dirname, `./keychain/${randomHash}.json`), JSON.stringify(key.toObject()), 'utf8', (err, done) => {
       if(err) throw new Error(err);
-      console.log(`Created new File ./keychain/${randomHash}.json for ${program.livenet ? 'Livenet' : 'Testnet'}`);
+      console.log(`➜ Created new File ./keychain/${randomHash}.json for ${program.livenet ? 'Livenet' : 'Testnet'}`);
     });
   })
 
